e7a9e63e9da68842d6556d0b1e2cf858d49bb8d2
Krishanthapathum <87825292+Krishanthapathum@users.noreply.github.com>
2024-01-18 22:40:00 +0530

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:addminus_sample/homepage.dart';
+import 'package:addminus_sample/new_home.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: NewHome(),
+    );
+  }
+}
